c7308caf93888ceb5b90eed8ea35bfcdbd4930c5
hacdan <david.mcclellan.p@gmail.com>
2023-11-13 16:20:00 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hacdan/FileMonitor
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"time"
+	"path/filepath"
+)
+
+func main() {
+	cwd, _ := os.Getwd()
+	tree := make(map[string]string)
+	err := walkTree(cwd, tree)	
+	if err != nil {
+		fmt.Println(err)
+	}
+	// TODO: Update to use arg or if no arg, cwd
+	for true {
+		newTree := make(map[string]string)
+		err := walkTree(cwd, newTree)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for dir, hash := range newTree {
+			if tree[dir] != hash {
+				fmt.Println(dir, ": ", tree[dir])
+				fmt.Println(dir, ": ", newTree[dir]) 
+				fmt.Println("Changed!")
+			}
+		}
+		tree = newTree
+		time.Sleep(5 * time.Second)
+	}
+	printMap(tree)
+}
+
+func printMap(hashes map[string]string) {
+	for k, v := range hashes {
+		fmt.Println(k, ": ", v)
+	}
+}
+
+func walkTree(dir string, hashes map[string]string) error {
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() && path != "" {
+				err = addHash(hashes, path)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func addHash(hashes map[string]string, path string) error {
+	hasher := sha256.New()
+	s, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	hasher.Write(s)
+	hashes[path] = hex.EncodeToString(hasher.Sum(nil))
+	return nil
+}
